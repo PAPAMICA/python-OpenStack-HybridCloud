150312c54540dd608d2d02868ee077ff7def11c4
Mickael Asseline <mickael@papamica.com>
2022-02-08 18:34:43 +0100

    letsgoooooo

---
+++ app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Instance
+instance_name = ""
+instance_flavor = ""
+instance_network= ""
+instance_securitygroup = ""
+instance_keypair = ""
+
+# Cloud
+cloud = ""
