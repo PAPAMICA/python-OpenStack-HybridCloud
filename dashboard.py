c0ca69511334056466fa5205d240b9d654d43631
Mickael Asseline <mickael@papamica.com>
2022-02-09 17:48:22 +0100

    Fix Flask test

--- dashboard.py
+++ dashboard.py
@@ -4,14 +4,11 @@ from flask import Flask, request, render_template, redirect
 dashboard_ip = "0.0.0.0"
 
 app = Flask(__name__)
-if __name__ == "__main__":
-    app.run(host=dashboard_ip, port=8086, debug=False)
 
 
-
-@app.route("/api/list/<cloud_name>", methods=['GET','POST'])
-def display_instances_list(cloud_name):
-    cloud_name = request.form["cloud"]
+@app.route("/list", methods=['GET','POST'])
+def display_instances_list():
+    cloud_name = request.form['cloud']
     cloud = api.cloud_connection(cloud_name)
     result = api.get_instances_list(cloud)
     return(result)
@@ -21,4 +18,6 @@ def display_instances_list(cloud_name):
 def home():
     return render_template("index.html")
 
+if __name__ == "__main__":
+    app.run(host=dashboard_ip, port=8086, debug=True)
     
