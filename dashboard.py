de3e11de9480605ee2184cf7c5891b3ba7e166b1
mouriermaxime <58846521+mouriermaxime@users.noreply.github.com>
2022-02-10 16:49:21 +0100

    result on one page

--- dashboard.py
+++ dashboard.py
@@ -9,15 +9,17 @@ dashbord_url = "https://hybridcloud.papamica.com"
 def hello():
     test = False
     if request.method == 'POST':
-        test = True
-    return render_template("index.html",test=test)
+        cloud_name = request.form["cloud"]
+        url = f'{dashbord_url}/api/list/{cloud_name}'
+        result = requests.get(url,verify=True)
+    return render_template("index.html",test=result.content)
 
-@app.route("/list", methods=['GET','POST'])
-def web_list_instances():
-    cloud_name = request.form["cloud"]
-    url = f'{dashbord_url}/api/list/{cloud_name}'
-    result = requests.get(url,verify=True)
-    return result.content
+# @app.route("/list", methods=['GET','POST'])
+# def web_list_instances():
+#     cloud_name = request.form["cloud"]
+#     url = f'{dashbord_url}/api/list/{cloud_name}'
+#     result = requests.get(url,verify=True)
+#     return result.content
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="8086", debug=True)
