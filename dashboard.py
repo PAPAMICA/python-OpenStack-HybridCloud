a4a65ef79ab6c2e90d8fc6419a8c69b50e5349d6
Mickael Asseline <mickael@papamica.com>
2022-02-11 00:01:37 +0100

    Test HTML

--- dashboard.py
+++ dashboard.py
@@ -11,17 +11,20 @@ dashbord_url = "https://hybridcloud.papamica.com"
 def list_instances():
     data = dict()
     if request.method == 'POST':
-        cloud_name = request.form.getlist('cloud')
-        if cloud_name:
-            cloud_name = "Infomaniak"
-        else:
-            cloud_name = "local"
-        url = f'{dashbord_url}/api/list/{cloud_name}'
-        result = requests.get(url,verify=True)
-        data = result.content
-        data = json.loads(data.decode('utf-8'))
-        if data == None:
-            data = {}   
+        if request.form['cloud']:
+            cloud_name = request.form.getlist('cloud')
+            if cloud_name:
+                cloud_name = "Infomaniak"
+            else:
+                cloud_name = "local"
+            url = f'{dashbord_url}/api/list/{cloud_name}'
+            result = requests.get(url,verify=True)
+            data = result.content
+            data = json.loads(data.decode('utf-8'))
+            if data == None:
+                data = {}   
+        if request.form['start']:
+            instance_name = request.form.getlist('start')
     return render_template("index.html",instances=data)
 
 # @app.route("/list", methods=['GET','POST'])
