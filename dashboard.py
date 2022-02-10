ff009302a8087bbc0406db49492488c9025fb7e9
Mickael Asseline <mickael@papamica.com>
2022-02-11 00:29:26 +0100

    Test HTML

--- dashboard.py
+++ dashboard.py
@@ -28,7 +28,7 @@ def home():
         elif request.form.get('start'):
             instance_name = request.form.getlist('start')
             print(instance_name)
-            url = f'{dashbord_url}/api/stop/{instance_name[0]}/{instance_name[1]}/stop'
+            url = f'{dashbord_url}/api/stop/{instance_name[0]}/{instance_name[1]}/start'
             result = requests.get(url,verify=True)
             reload_list(cloud_name)
     return render_template("index.html")
