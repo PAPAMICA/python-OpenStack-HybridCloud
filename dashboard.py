25384282231961de613c5f444167f0c34788e77d
Mickael Asseline <mickael@papamica.com>
2022-02-10 17:31:29 +0100

    test html

--- dashboard.py
+++ dashboard.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from flask import Flask, request, render_template, redirect
 app = Flask(__name__)
 import api
@@ -12,11 +13,13 @@ def hello():
         cloud_name = request.form["cloud"]
         url = f'{dashbord_url}/api/list/{cloud_name}'
         result = requests.get(url,verify=True)
-        test = result.content
-        test_json = test.json()
+        data = result.content
+        data = data.decode('utf8').replace("'", '"')
+        data = json.loads(data)
+        result = json.dumps(data, indent=4, sort_keys=True)
         # if test == None:
         #     test = {}   
-    return render_template("index.html",test=test_json)
+    return render_template("index.html",instances=result)
 
 # @app.route("/list", methods=['GET','POST'])
 # def web_list_instances():
