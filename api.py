025516bd713d757781f689a0ee35a84b528b9c73
mouriermaxime <58846521+mouriermaxime@users.noreply.github.com>
2022-02-10 17:39:50 +0100

    create_keypair

--- api.py
+++ api.py
@@ -8,8 +8,15 @@ def display_instances_list(cloud_name):
     result = openstack_api.get_instances_list(cloud)
     return(result)
 
-@app.route("/api/inst_info/<cloud_name>/<server_name>")
+@app.route("/api/list/<cloud_name>/<server_name>")
 def display_instance_information(cloud_name,server_name):
     cloud  = openstack_api.cloud_connection(cloud_name)
     result = openstack_api.get_instance_information(cloud, server_name)
+    return(result)
+
+@app.route("/api/<cloud_name>/new_keypair")
+def create_keypair(cloud_name):
+    keypair_name = request.headers['keypair_name']
+    cloud  = openstack_api.cloud_connection(cloud_name)
+    result = openstack_api.create_keypair(cloud, keypair_name)
     return(result)
