2bdc0c5c79929b5ca0613be12144b1907cd3914f
mouriermaxime <58846521+mouriermaxime@users.noreply.github.com>
2022-02-10 17:51:12 +0100

    test create instance

---
+++ test.py
@@ -0,0 +1,2 @@
+import openstack_api
+openstack_api.create_instance("Infomaniak", "test_instance","Ubuntu 20.04 LTS Focal Fossa", "a1-ram2-disk20-perf1", "k8s-network", "keypair_test", "k8s-masters")
