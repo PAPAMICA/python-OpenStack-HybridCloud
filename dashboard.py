afcef0c7c6f14e067d58cf9267cb690cc77d2698
Mickael Asseline <mickael@papamica.com>
2022-02-10 16:00:34 +0100

    fix ip

--- dashboard.py
+++ dashboard.py
@@ -3,7 +3,7 @@ from flask import Flask, request, render_template, redirect
 app = Flask(__name__)
 import api
 
-dashbord_url = "0.0.0.0"
+dashbord_url = "https://hybridcloud.papamica.com"
 
 @app.route("/", methods=['GET','POST'])
 def hello():
@@ -12,7 +12,7 @@ def hello():
 @app.route("/list/", methods=['GET','POST'])
 def web_list_instances():
     cloud_name = request.form["cloud"]
-    url = f'http://{dashbord_url}:8086/api/list/{cloud_name}'
+    url = f'{dashbord_url}/api/list/{cloud_name}'
     result = requests.get(url)
     return result.content
 
