c966bded47118a7824f683a656aeb765668a38b3
Mickael Asseline <mickael@papamica.com>
2022-02-10 23:04:57 +0100

    Test HTML

--- dashboard.py
+++ dashboard.py
@@ -11,7 +11,11 @@ dashbord_url = "https://hybridcloud.papamica.com"
 def list_instances():
     data = dict()
     if request.method == 'POST':
-        cloud_name = request.form["cloud"]
+        cloud_name = request.form.getlist('cloud')
+        if cloud_name:
+            cloud_name = "Infomaniak"
+        else:
+            cloud_name = "local"
         url = f'{dashbord_url}/api/list/{cloud_name}'
         result = requests.get(url,verify=True)
         data = result.content
