fdd48259c720a6ca8e3fa1ab90f22bfe1a6d5695
Mickael Asseline <mickael@papamica.com>
2022-02-11 01:30:21 +0100

    Test HTML

--- dashboard.py
+++ dashboard.py
@@ -33,28 +33,28 @@ def home():
             url = f'{dashbord_url}/api/{cloud_name[0]}/{instance_name[0]}/start'
             result = requests.get(url)
             #print(result, flush=True, file=sys.stdout)
-            reload_list(cloud_name)
+            reload_list(cloud_name[0])
         elif request.form.get('reboot'):
             instance_name = request.form.getlist('reboot')
             cloud_name = request.form.getlist('cloud_name')
             url = f'{dashbord_url}/api/{cloud_name[0]}/{instance_name[0]}/reboot'
             result = requests.get(url)
             #print(result, flush=True, file=sys.stdout)
-            reload_list(cloud_name)
+            reload_list(cloud_name[0])
         elif request.form.get('stop'):
             instance_name = request.form.getlist('stop')
             cloud_name = request.form.getlist('cloud_name')
             url = f'{dashbord_url}/api/{cloud_name[0]}/{instance_name[0]}/stop'
             result = requests.get(url)
             #print(result, flush=True, file=sys.stdout)
-            reload_list(cloud_name)
+            reload_list(cloud_name[0])
         elif request.form.get('destroy'):
             instance_name = request.form.getlist('destroy')
             cloud_name = request.form.getlist('cloud_name')
             url = f'{dashbord_url}/api/{cloud_name[0]}/{instance_name[0]}'
             result = requests.delete(url)
             #print(result, flush=True, file=sys.stdout)
-            reload_list(cloud_name)
+            reload_list(cloud_name[0])
     return render_template("index.html")
 
 def reload_list(cloud_name):
