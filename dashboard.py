9469bc09f63ac7610bcf59965da0550a96507009
Mickael Asseline <mickael@papamica.com>
2022-02-11 02:40:29 +0100

    Test HTML

--- dashboard.py
+++ dashboard.py
@@ -14,21 +14,7 @@ dashbord_url = "https://hybridcloud.papamica.com"
 def home():
     data = dict()
     if request.method == 'POST':
-        if request.form.get('cloud'):
-            cloud_name = request.form.getlist('cloud')
-            if cloud_name:
-                cloud_name = "Infomaniak"
-            else:
-                cloud_name = "local"
-            url = f'{dashbord_url}/api/list/{cloud_name}'
-            result = requests.get(url,verify=True)
-            data = result.content
-            data = json.loads(data.decode('utf-8'))
-            if data == None:
-                data = {}   
-            return render_template("index.html",instances=data, cloud_name=cloud_name)
-
-        elif request.form.get('start'):
+        if request.form.get('start'):
             instance_name = request.form.getlist('start')
             cloud_name = request.form.getlist('cloud_name')
             url = f'{dashbord_url}/api/{cloud_name[0]}/{instance_name[0]}/start'
@@ -60,7 +46,21 @@ def home():
             #print(result, flush=True, file=sys.stdout)
             time.sleep(1)
             reload_list(cloud_name[0])
-    #return render_template("index.html")
+
+        elif request.form.get('cloud'):
+            cloud_name = request.form.getlist('cloud')
+            if cloud_name:
+                cloud_name = "Infomaniak"
+            else:
+                cloud_name = "local"
+            url = f'{dashbord_url}/api/list/{cloud_name}'
+            result = requests.get(url,verify=True)
+            data = result.content
+            data = json.loads(data.decode('utf-8'))
+            if data == None:
+                data = {}   
+            return render_template("index.html",instances=data, cloud_name=cloud_name)
+    return render_template("index.html")
 
 def reload_list(cloud_name):
     url = f'{dashbord_url}/api/list/{cloud_name}'
