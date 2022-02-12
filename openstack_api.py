bf3899815281d090be38466cdcd7351d89a2a8a8
Mickael Asseline <mickael@papamica.com>
2022-02-12 11:32:00 +0100

    Test HTML

--- openstack_api.py
+++ openstack_api.py
@@ -280,7 +280,8 @@ def create_instance(cloud, instance_name,instance_image, instance_flavor, instan
     server = cloud.compute.wait_for_server(server)
     print("TEST")
     server = cloud.compute.add_security_group_to_server(server, security_group)
-    print(get_instance_information(cloud, instance_name))
+    #get_instance_information(cloud, instance_name)
+    print("TEST")
 
 
 # Start instance
