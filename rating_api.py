735fc8d247c18d891e040856ee95f652302b655e
Mickael Asseline <mickael@papamica.com>
2022-02-12 17:04:36 +0100

    Add rating

---
+++ rating_api.py
@@ -0,0 +1,16 @@
+from re import X
+import subprocess
+
+def get_billing(cloud_name):
+    source = subprocess.getoutput(f'source /openrc/{cloud_name}')
+    lastmonth = ['m-1', '-b $(date -d "`date +%Y%m01` -1 month" +%Y-%m-%d) -e $(date -d "`date +%Y%m01` -1 day" +%Y-%m-%d)']
+    thismonth = ['m', '-b $(date -d "`date +%Y%m01`" +%Y-%m-%d) -e $(date -d "`date +%Y%m01` +1 month -1 day" +%Y-%m-%d)']
+    lastday = ['d-1', '-b $(date -d "`date +%Y-%m-%d` -1 day" +%Y-%m-%d) -e $(date -d "`date +%Y-%m-%d`" +%Y-%m-%d)']
+    thisday = ['d', '-b $(date -d "`date +%Y-%m-%d`" +%Y-%m-%d)']
+    delay = [lastmonth, thismonth, lastday, thisday]
+    result = dict()
+    for d in delay:
+        data = subprocess.getoutput(f'openstack rating dataframes get {d[1]} -c Resources -f json | jq \'map(.Resources[].rating | tonumber) | add | . / 50\' ')
+        data = round(float(data),2)
+        result[d[0]] = data
+    return result
