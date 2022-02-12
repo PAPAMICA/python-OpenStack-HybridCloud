a42ac0c8702e51d306dc3450f5a941355d52c038
Mickael Asseline <mickael@papamica.com>
2022-02-12 21:20:30 +0100

    Add rating to api

--- rating_api.py
+++ rating_api.py
@@ -18,10 +18,10 @@ def get_billing(cloud_name):
     os.putenv('OS_REGION_NAME', OS_REGION_NAME)   
     os.putenv('OS_PROJECT_DOMAIN_NAME', OS_PROJECT_DOMAIN_NAME)  
     os.putenv('OS_USER_DOMAIN_NAME', OS_USER_DOMAIN_NAME)  
-    lastmonth = ['m-1', '-b $(date -d "`date +%Y%m01` -1 month" +%Y-%m-%d) -e $(date -d "`date +%Y%m01` -1 day" +%Y-%m-%d)']
-    thismonth = ['m', '-b $(date -d "`date +%Y%m01`" +%Y-%m-%d) -e $(date -d "`date +%Y%m01` +1 month -1 day" +%Y-%m-%d)']
-    lastday = ['d-1', '-b $(date -d "`date +%Y-%m-%d` -1 day" +%Y-%m-%d) -e $(date -d "`date +%Y-%m-%d`" +%Y-%m-%d)']
-    thisday = ['d', '-b $(date -d "`date +%Y-%m-%d`" +%Y-%m-%d)']
+    lastmonth = ['Last month', '-b $(date -d "`date +%Y%m01` -1 month" +%Y-%m-%d) -e $(date -d "`date +%Y%m01` -1 day" +%Y-%m-%d)']
+    thismonth = ['This month', '-b $(date -d "`date +%Y%m01`" +%Y-%m-%d) -e $(date -d "`date +%Y%m01` +1 month -1 day" +%Y-%m-%d)']
+    lastday = ['Yesterday', '-b $(date -d "`date +%Y-%m-%d` -1 day" +%Y-%m-%d) -e $(date -d "`date +%Y-%m-%d`" +%Y-%m-%d)']
+    thisday = ['Today', '-b $(date -d "`date +%Y-%m-%d`" +%Y-%m-%d)']
     delay = [lastmonth, thismonth, lastday, thisday]
     result = dict()
     for d in delay:
