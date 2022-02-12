218efd17ccd4bbfd2b071d855e985410e5a3b221
mouriermaxime <58846521+mouriermaxime@users.noreply.github.com>
2022-02-12 13:16:17 +0100

    débile

--- dashboard.py
+++ dashboard.py
@@ -90,7 +90,7 @@ def home():
                     }
             url = f'{dashbord_url}/api/{cloud_name}/new_instance?api_key=1234'
             r = requests.post(url,data=json.dumps(body),headers=headers,verify=True)
-            return r, data, body 
+            return r, body 
             #data = data.content
             #data = json.loads(data.decode('utf-8'))
             #result[cloud_name] = data
