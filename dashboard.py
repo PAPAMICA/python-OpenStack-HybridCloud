4aa5cced59033d9c65d01f09dea08881657e4da0
Mickael Asseline <mickael@papamica.com>
2022-02-13 13:55:02 +0100

    Add destroy button for apikey

--- dashboard.py
+++ dashboard.py
@@ -73,7 +73,9 @@ def home():
         
         elif request.form.get('delete_apikey'):
             key_name = request.form.getlist('key_name')
-            bdd.delete_api_key(key_name)
+            print(key_name, flush=True, file=sys.stdout)
+            test = bdd.delete_api_key(key_name)
+            print(test, flush=True, file=sys.stdout)
             result = bdd.list_api_key()
             return render_template("index.html", list_api_key=result, billing=billing)
         
